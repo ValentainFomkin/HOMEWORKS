ea24aaeb24f30d2c8120b9508d7320723e624dd6
ValentainFomkin <valentain1625@mail.ru>
2022-10-12 16:02:36 +0300

    Clear function App and create components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
+
+export function App() {
+    return (
+        <>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
+    );
+}
+
+
