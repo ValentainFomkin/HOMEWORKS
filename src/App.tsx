b4d65183ee14a78b50f096230de40f7f0c44ee94
ValentainFomkin <valentain1625@mail.ru>
2022-10-13 18:37:33 +0300

    create right more button with onClick

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,12 @@ export function App() {
     //     console.log(name)
     // }
 
-    const foo1=(name: number)=>{
+    const foo1=()=>{
         //ничего не передавать, но в консоли должно показаться 100200
-        console.log(name)
+        console.log(100200)
     }
-    const foo2=(name: number)=>{
-        console.log(name)
+    const foo2=(num: number)=>{
+        console.log(num)
 
     }
 
@@ -34,7 +34,7 @@ export function App() {
             {/*<button onClick={(event) =>onClickHandler('Ivan')}>MyYouTubeChanel-2</button>*/}
             {/*<button onClick={()=>onClickHandler('some info')}>MyYouTubeChanel-3</button>*/}
 
-        <button onClick={(event)=>foo1(100200)}>1</button>
+        <button onClick={foo1}>1</button>
         <button onClick={()=>foo2(100200)}>2</button>
         </div>
 
